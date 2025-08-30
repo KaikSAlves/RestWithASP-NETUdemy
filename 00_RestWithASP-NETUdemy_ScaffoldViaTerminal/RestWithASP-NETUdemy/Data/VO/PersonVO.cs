6d99b5267e14ea6efb9751c0b9d92cfec38f38d0
KaikSAlves <souzaalveskaik@gmail.com>
2025-08-30 12:30:32 -0300

    Custom Serialization

--- 00_RestWithASP-NETUdemy_ScaffoldViaTerminal/RestWithASP-NETUdemy/Data/VO/PersonVO.cs
+++ 00_RestWithASP-NETUdemy_ScaffoldViaTerminal/RestWithASP-NETUdemy/Data/VO/PersonVO.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using RestWithASP_NETUdemy.Model.Base;
 
@@ -7,14 +8,20 @@ namespace RestWithASP_NETUdemy.Data.VO;
 public class PersonVO
 {
     
+    [JsonPropertyName("code")]
     public long Id { get; set; }
     
+    [JsonPropertyName("name")]
     public string FirstName { get; set; }
 
+    [JsonPropertyName("last_name")]
     public string LastName { get; set; }
 
+    
+    [JsonIgnore]
     public string Address { get; set; }
     
+    [JsonPropertyName("sex")]
     public string Gender {get; set;}
     
 }
